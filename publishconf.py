a568364617bb88c5e49aa61c1d0cf41d196f3a51
0x4448 <86135470+0x4448@users.noreply.github.com>
2022-08-08 13:37:42 -0700

    Fix sys.path for running `make github`

--- publishconf.py
+++ publishconf.py
@@ -1,4 +1,8 @@
-from pelicanconf import *  # noqa: F401,F403
+import os
+import sys
+
+sys.path.insert(0, os.curdir)
+from pelicanconf import *  # noqa: E402,F401,F403
 
 SITEURL = "https://0x4448.github.io"
 RELATIVE_URLS = False
